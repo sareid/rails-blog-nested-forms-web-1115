c67de3a70041a8d2ca5c3f1fc7fff5783b65e98a
sareid <andrew.reid@flatironschool.com>
2016-01-07 17:12:16 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,9 @@ class Post < ActiveRecord::Base
   has_many :post_tags
   has_many :tags, :through => :post_tags
 
+  accepts_nested_attributes_for :tags
+
+
+
   validates_presence_of :name, :content
 end
